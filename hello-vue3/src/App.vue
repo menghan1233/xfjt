fa9a92af9fc349d0121dacdc678812ac92963b46
menghan1233 <42926575+menghan1233@users.noreply.github.com>
2021-08-14 20:44:51 +0800

    Add files via upload

---
+++ hello-vue3/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+@import url(./assets/css/reset.css);
+}
+</style>
---
+++ hello-vue3/src/components/home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="ss">
+   {{w}} {{we}}
+  </div>
+  <van-button type="primary" @click="toOther">主要按钮</van-button>
+</template>
+
+<script>
+import {getCurrentInstance, onMounted, reactive, ref, toRef, toRefs} from "vue";
+import {  useRouter,useRoute} from 'vue-router'
+export default {
+  name: 'home',
+  props: {
+    msg: String
+  },
+ setup(){
+    const that = getCurrentInstance()
+    const obj = {key:'11'}
+   const router = useRouter()
+   const route = useRoute()
+   // const route = useRoute()
+   let tor = ref(0)
+   let toR = toRef(obj,'toR')
+    const state = reactive({
+      w:'www',
+      we:'we1'
+    })
+   onMounted(() => {
+     console.log("onMounted", tor,
+         toR);
+   });
+   const toOther = () => {
+     console.log(router,route)
+     router.push({path:'./ceshi'})
+   }
+   return {
+     router,
+     route,
+      tor,
+     toR,
+     toOther,
+      ...toRefs(state)
+   }
+ },
+
+}
+</script>
+
+<style lang="scss"  scoped>
+ .ss{
+    font-size: 10px;
+ }
+</style>
